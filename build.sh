6b4a4cde36641adb7a1555b2c7e500aed2033c74
Illia Polosukhin <ilblackdragon@gmail.com>
2020-02-05 11:13:53 -0300

    Fixing compilation for wasm by uncommenting crate-type. Removed sed from build.sh as it's not needed anymore with new clion. neardev is in gitignore to prevent leaking keys

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-sed -i "s|# crate\-type|crate\-type|g" Cargo.toml
-
 RUSTFLAGS='-C link-arg=-s' cargo +stable build --target wasm32-unknown-unknown --release
 mkdir -p res
 cp target/wasm32-unknown-unknown/release/near_evm.wasm ./res/
-
-sed -i "s|crate\-type|# crate\-type|g" Cargo.toml
 
 # wasm-opt -Oz --output ./res/near_evm.wasm ./res/near_evm.wasm
 # rm -rf target

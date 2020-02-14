8a2b0df73fd50341d962e0e48c5803743fbb4f5b
James Prestwich <james@prestwi.ch>
2020-02-13 23:22:12 -0800

    tests: fix tests for new balance

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -17,17 +17,17 @@ fn test_sends() {
     test_utils::run_test(100, |contract| {
         let evm_acc = hex::encode(utils::near_account_id_to_evm_address("evmGuy").0);
 
-        assert_eq!(contract.balance_of_near_account("owner1".to_string()), 0);
+        assert_eq!(contract.balance_of_near_account("owner1".to_string()).0, 0);
         contract.add_near();
-        assert_eq!(contract.balance_of_near_account("owner1".to_string()), 100);
+        assert_eq!(contract.balance_of_near_account("owner1".to_string()).0, 100);
 
-        contract.move_funds_to_evm_address(evm_acc.clone(), 50);
-        assert_eq!(contract.balance_of_near_account("owner1".to_string()), 50);
-        assert_eq!(contract.balance_of_evm_address(evm_acc), 50);
+        contract.move_funds_to_evm_address(evm_acc.clone(), utils::Balance(50));
+        assert_eq!(contract.balance_of_near_account("owner1".to_string()).0, 50);
+        assert_eq!(contract.balance_of_evm_address(evm_acc).0, 50);
 
-        contract.move_funds_to_near_account("someGuy".to_string(), 25);
-        assert_eq!(contract.balance_of_near_account("owner1".to_string()), 25);
-        assert_eq!(contract.balance_of_near_account("someGuy".to_string()), 25);
+        contract.move_funds_to_near_account("someGuy".to_string(), utils::Balance(25));
+        assert_eq!(contract.balance_of_near_account("owner1".to_string()).0, 25);
+        assert_eq!(contract.balance_of_near_account("someGuy".to_string()).0, 25);
         // TODO: assert contract NEAR balance
     })
 }
@@ -36,16 +36,16 @@ fn test_sends() {
 fn test_deploy_with_nonce() {
     test_utils::run_test(0, |contract| {
         let evm_acc = hex::encode(utils::near_account_id_to_evm_address("owner1").0);
-        assert_eq!(contract.nonce_of_near_account("owner1".to_string()), 0);
-        assert_eq!(contract.nonce_of_evm_address(evm_acc.clone()), 0);
+        assert_eq!(contract.nonce_of_near_account("owner1".to_string()).0, 0);
+        assert_eq!(contract.nonce_of_evm_address(evm_acc.clone()).0, 0);
 
         contract.deploy_code(TEST.to_string());
-        assert_eq!(contract.nonce_of_near_account("owner1".to_string()), 1);
-        assert_eq!(contract.nonce_of_evm_address(evm_acc.clone()), 1);
+        assert_eq!(contract.nonce_of_near_account("owner1".to_string()).0, 1);
+        assert_eq!(contract.nonce_of_evm_address(evm_acc.clone()).0, 1);
 
         contract.deploy_code(TEST.to_string()); // at a different address
-        assert_eq!(contract.nonce_of_near_account("owner1".to_string()), 2);
-        assert_eq!(contract.nonce_of_evm_address(evm_acc.clone()), 2);
+        assert_eq!(contract.nonce_of_near_account("owner1".to_string()).0, 2);
+        assert_eq!(contract.nonce_of_evm_address(evm_acc.clone()).0, 2);
     })
 }
 
@@ -53,12 +53,12 @@ fn test_deploy_with_nonce() {
 fn test_internal_create() {
     test_utils::run_test(0, |contract| {
         let test_addr = contract.deploy_code(TEST.to_string());
-        assert_eq!(contract.nonce_of_evm_address(test_addr.clone()), 0);
+        assert_eq!(contract.nonce_of_evm_address(test_addr.clone()).0, 0);
 
         // This should increment the nonce of the deploying contract
         let (input, _) = soltest::functions::deploy_new_guy::call(8);
         let raw = contract.call_contract(test_addr.clone(), hex::encode(input));
-        assert_eq!(contract.nonce_of_evm_address(test_addr.clone()), 1);
+        assert_eq!(contract.nonce_of_evm_address(test_addr.clone()).0, 1);
 
         let sub_addr = raw[24..64].to_string();
         let (new_input, _) = subcontract::functions::a_number::call();
@@ -74,7 +74,7 @@ fn test_internal_create() {
 fn test_deploy_and_transfer() {
     test_utils::run_test(100, |contract| {
         let test_addr = contract.deploy_code(TEST.to_string());
-        assert_eq!(contract.balance_of_evm_address(test_addr.clone()), 100);
+        assert_eq!(contract.balance_of_evm_address(test_addr.clone()).0, 100);
 
         // This should increment the nonce of the deploying contract
         // There is 100 attached to this that should be passed through
@@ -83,8 +83,8 @@ fn test_deploy_and_transfer() {
 
         // The sub_addr should have been transferred 100 monies
         let sub_addr = raw[24..64].to_string();
-        assert_eq!(contract.balance_of_evm_address(test_addr), 100);
-        assert_eq!(contract.balance_of_evm_address(sub_addr), 100);
+        assert_eq!(contract.balance_of_evm_address(test_addr).0, 100);
+        assert_eq!(contract.balance_of_evm_address(sub_addr).0, 100);
     })
 }
 
@@ -94,7 +94,7 @@ fn test_deploy_with_value() {
         // This test is identical to the previous one
         // As we expect behavior to be the same.
         let test_addr = contract.deploy_code(TEST.to_string());
-        assert_eq!(contract.balance_of_evm_address(test_addr.clone()), 100);
+        assert_eq!(contract.balance_of_evm_address(test_addr.clone()).0, 100);
 
         // This should increment the nonce of the deploying contract
         // There is 100 attached to this that should be passed through
@@ -103,8 +103,8 @@ fn test_deploy_with_value() {
 
         // The sub_addr should have been transferred 100 monies
         let sub_addr = raw[24..64].to_string();
-        assert_eq!(contract.balance_of_evm_address(test_addr), 100);
-        assert_eq!(contract.balance_of_evm_address(sub_addr), 100);
+        assert_eq!(contract.balance_of_evm_address(test_addr).0, 100);
+        assert_eq!(contract.balance_of_evm_address(sub_addr).0, 100);
     })
 }
 
@@ -114,14 +114,14 @@ fn test_contract_to_eoa_transfer() {
         // This test is identical to the previous one
         // As we expect behavior to be the same.
         let test_addr = contract.deploy_code(TEST.to_string());
-        assert_eq!(contract.balance_of_evm_address(test_addr.clone()), 100);
+        assert_eq!(contract.balance_of_evm_address(test_addr.clone()).0, 100);
 
         let (input, _) = soltest::functions::return_some_funds::call();
         let raw = contract.call_contract(test_addr.clone(), hex::encode(input));
 
         let sender_addr = raw[24..64].to_string();
-        assert_eq!(contract.balance_of_evm_address(test_addr), 150);
-        assert_eq!(contract.balance_of_evm_address(sender_addr), 50);
+        assert_eq!(contract.balance_of_evm_address(test_addr).0, 150);
+        assert_eq!(contract.balance_of_evm_address(sender_addr).0, 50);
     })
 }
 

02dc4d6f895666c0fdc92ac1247eccc39c680097
Illia Polosukhin <ilblackdragon@gmail.com>
2020-04-27 09:29:22 -0700

    Update versions of near-vm-logic and pin nightly version to the same as nearcore

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RUSTFLAGS='-C link-arg=-s' cargo +stable build --target wasm32-unknown-unknown --release
+RUSTFLAGS='-C link-arg=-s' cargo +stable build -p near-evm --target wasm32-unknown-unknown --release
 mkdir -p res
 cp target/wasm32-unknown-unknown/release/near_evm.wasm ./res/
 

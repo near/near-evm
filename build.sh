a4537bbc902013987e3a6201db4530882d2fb0ce
Emily Williams <emag3m@gmail.com>
2020-06-06 11:38:48 -0400

    run ./build.sh successfully

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RUSTFLAGS='-C link-arg=-s' cargo +stable build -p near-evm --target wasm32-unknown-unknown --release
+RUSTFLAGS='-C link-arg=-s' cargo +stable build -p near-evm --target wasm32-unknown-unknown --release || exit 1
 mkdir -p res
 cp target/wasm32-unknown-unknown/release/near_evm.wasm ./res/
 

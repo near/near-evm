b70fb5568da9422307641e8e96eda9975a093948
Emily Williams <emag3m@gmail.com>
2020-06-11 11:03:39 -0400

    Update test contracts with create2 & selfdestruct

--- src/tests/contracts/SolTests.sol
+++ src/tests/contracts/SolTests.sol
@@ -2,16 +2,15 @@ pragma solidity ^0.5.8;
 
 contract ExposesBalance {
   function balance() public view returns (uint256) {
-    return address(this).balance;
+      return address(this).balance;
   }
 
   // Unpermissioned. Don't deploy for real :D
   function transferTo(address _recipient, uint256 _amount) public returns (uint256) {
-    address(uint160(_recipient)).transfer(_amount);
-    return balance();
+      address(uint160(_recipient)).transfer(_amount);
+      return balance();
   }
 }
-
 
 contract SolTests is ExposesBalance {
     constructor() public payable {}
@@ -27,32 +26,58 @@ contract SolTests is ExposesBalance {
     }
 
     function payNewGuy(uint256 _aNumber) public payable returns (address, uint256) {
-      SubContract _newGuy = (new SubContract).value(msg.value)(_aNumber);
-      return (address(_newGuy), msg.value);
+        SubContract _newGuy = (new SubContract).value(msg.value)(_aNumber);
+        return (address(_newGuy), msg.value);
     }
 
     function returnSomeFunds() public payable returns (address, uint256) {
-      address(msg.sender).transfer(msg.value / 2);
-      return (msg.sender, msg.value);
+        address(msg.sender).transfer(msg.value / 2);
+        return (msg.sender, msg.value);
     }
 
     function emitIt(uint256 _aNumber) public returns (bool) {
-      emit SomeEvent(_aNumber);
-      return true;
+        emit SomeEvent(_aNumber);
+        return true;
     }
 }
 
 contract SubContract is ExposesBalance {
-
     uint256 public aNumber = 6;
 
     constructor(uint256 _aNumber) public payable {
-      aNumber = _aNumber;
+        aNumber = _aNumber;
     }
 
     function aFunction() public pure returns (bool) {
-      return true;
+        return true;
     }
 
     function () external payable {}
+}
+
+contract Create2Factory {
+    function deploy(bytes32 _salt, bytes memory _contractBytecode) public returns (address addr) {
+        assembly {
+            addr := create2(0, add(_contractBytecode, 0x20), mload(_contractBytecode), _salt)
+        }
+    }
+}
+
+contract SelfDestruct {
+    address public storedAddress;
+    uint public storedUint;
+
+    function () external payable {}
+
+    function storeAddress() public {
+        storedAddress = msg.sender;
+    }
+
+    function storeUint(uint _number) public {
+        storedUint = _number;
+    }
+
+    function destruction() public returns (bool) {
+        selfdestruct(msg.sender);
+    }
 }

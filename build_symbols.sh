1e8cba89712bd0a399d9c4b03df876088aac4a83
Arto Bendiken <arto@near.org>
2021-03-05 14:29:47 +0000

    Make build scripts more robust.

--- build_symbols.sh
+++ build_symbols.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
-cargo +nightly build -p near-evm --lib --target wasm32-unknown-unknown --release --no-default-features --features=contract -Z avoid-dev-deps || exit 1
+cargo +nightly build -p near-evm --lib --target wasm32-unknown-unknown --release --no-default-features --features=contract -Z avoid-dev-deps
 
 mkdir -p res
 cp target/wasm32-unknown-unknown/release/near_evm.wasm ./res/near_evm_symbols.wasm
